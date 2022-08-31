6ce9aae9bce19de85f01bdd2470044d88e83e841
ci7lus <7887955+ci7lus@users.noreply.github.com>
2022-08-31 23:46:05 +0900

    :alien: React18のcreateRoot

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { MantineProvider } from "@mantine/core"
 import React from "react"
-import ReactDOM from "react-dom"
+import { createRoot } from "react-dom/client"
 import App from "./App"
 
-ReactDOM.render(
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <MantineProvider
       withGlobalStyles
@@ -12,6 +13,5 @@ ReactDOM.render(
     >
       <App />
     </MantineProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 )
